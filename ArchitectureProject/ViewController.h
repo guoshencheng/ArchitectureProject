c7d56f568c66da5cf526f2e8438a0f416694a136
guoshencheng <648772021@qq.com>
2015-06-18 09:47:00 +0800

    Architecture commit

--- ArchitectureProject/ViewController.h
+++ ArchitectureProject/ViewController.h
@@ -7,8 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "ApiService.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <ApiServiceDelegate>
 
 
 @end
--- ArchitectureProject/ViewController.m
+++ ArchitectureProject/ViewController.m
@@ -16,12 +16,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+//    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 320, 568)];
+//    label.text = NSLocalizedString(@"author", nil);
+//    [self.view addSubview:label];
+    [[ApiService serviceWithDelegate:self] sendJSONRequest:[ApiRequest requestForTestServer]];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)service:(ApiService *)service didFinishRequest:(ApiRequest *)request withResponse:(ApiResponse *)response {
+    NSLog(@"%@", response);
 }
 
 @end
